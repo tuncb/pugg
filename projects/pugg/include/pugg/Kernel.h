1184f34989774070e715ad6ee02c121684a54a53
KOLANICH <kolan_n@mail.ru>
2021-07-20 17:01:50 +0300

    Added plugins autodiscovery.

--- projects/pugg/include/pugg/Kernel.h
+++ projects/pugg/include/pugg/Kernel.h
@@ -4,14 +4,29 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #pragma once
+#include <cstring>
 #include <string>
 #include <map>
 #include <vector>
+#include <iostream>
+
 
 #ifdef _WIN32
 	#include <windows.h>
 	#include <cwchar>
 #endif
+
+#ifdef WITH_STD_FILESYSTEM
+#include <filesystem>
+#else
+#include <dirent.h> // some impls have broken impl of std::filesystem
+#include <stdlib.h>
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include "Driver.h"
 #include "Plugin.h"
@@ -81,6 +96,126 @@ public:
 
 		server->drivers()[driver->name()] = driver;
 		return true;
+	}
+
+
+	#ifdef WIN32
+		static const constexpr wchar_t* platformDllExt = L".dll";
+	#else
+		static const constexpr char* platformDllExt = ".so";
+	#endif
+
+	void discover_backends(
+	#ifdef WIN32
+		std::wstring
+	#else
+		std::string
+	#endif
+	backendsDirPath
+	){
+		#ifndef WITH_STD_FILESYSTEM
+
+		#ifdef WIN32
+		auto openDirF = _wopendir;
+		using DIR_T = _WDIR;
+		using StrT = std::wstring;
+		auto readDirFunc = _wreaddir;
+		auto slenFunc = std::wcslen;
+		auto &debugMsgSteam = std::wcerr;
+		auto statF = wstat;
+		auto closeDirF = _wclosedir;
+		#else
+		auto openDirF = opendir;
+		using DIR_T = DIR;
+		using StrT = std::string;
+		auto readDirFunc = readdir;
+		auto slenFunc = std::strlen;
+		auto &debugMsgSteam = std::cerr;
+		auto statF = stat;
+		auto closeDirF = closedir;
+		#endif
+		DIR_T *dir = openDirF(backendsDirPath.data());
+
+		#else
+		std::filesystem::directory_iterator dir(backendsDirPath.data());
+		#endif
+
+		auto backendPath = backendsDirPath +
+		#ifdef WIN32
+		L"/"
+		#else
+		"/"
+		#endif
+		;
+		auto backendsPrefixLen = backendPath.size();
+		backendPath.reserve(PATH_MAX);
+		if(dir) {
+			#ifndef WITH_STD_FILESYSTEM
+			StrT resolved;
+			resolved.reserve(PATH_MAX);
+
+			for(auto *ent = readDirFunc(dir);ent;ent = readDirFunc(dir)){
+			#else
+			for(std::filesystem::directory_entry &ent: dir){
+			#endif
+				#ifdef WITH_STD_FILESYSTEM
+				try{
+					auto filename = ent.path.c_str();
+					auto backendPath = filesystem::canonical(ent.path);
+					std::cerr << "Backend library candidate path: " << backendPath << std::endl;
+				#else
+				auto filename = ent->d_name;
+				struct stat path_stat;
+				resolved.resize(PATH_MAX);
+				backendPath.resize(backendsPrefixLen);
+				backendPath += filename;
+
+				debugMsgSteam << "Backend library candidate path: " << backendPath << std::endl;
+
+				if(
+					#ifdef WIN32
+					GetFullPathNameW(backendPath.data(), resolved.size(), resolved.data(), nullptr)
+					#else
+					realpath(backendPath.data(), const_cast<char *>(resolved.data()))
+					#endif
+				){
+					auto len = slenFunc(resolved.data());
+					resolved.resize(len);
+				#endif
+					debugMsgSteam << "Backend library candidate resolved path: " << resolved << std::endl;
+
+					#ifdef WITH_STD_FILESYSTEM
+					#else
+					statF(resolved.data(), &path_stat);
+					debugMsgSteam << ent->d_name << "(" << backendPath << ") -> " << resolved << " : " << path_stat.st_mode << std::endl;
+					if(!S_ISREG(path_stat.st_mode)){
+						debugMsgSteam << "Backend library is not a usual file: mode is " << path_stat.st_mode << std::endl;
+						continue;
+					}
+					auto extensionPos = resolved.rfind(platformDllExt);
+					auto expectedExtensionPos = len - (slenFunc(platformDllExt));
+					if(extensionPos == expectedExtensionPos){
+						this->load_plugin(resolved);
+					}
+					#endif
+
+				#ifdef WITH_STD_FILESYSTEM
+				}catch(std::exception &ex){
+					auto err = ex.what();
+				#else
+				}else{
+					#ifdef WIN32
+					auto err = GetLastError();
+					#else
+					auto err = strerror(errno);
+					#endif
+				#endif
+					debugMsgSteam << "Error resolving " << filename << " : ";
+					debugMsgSteam << err << std::endl;
+				}
+			}
+			closeDirF(dir);
+		}
 	}
 
 	template <class DriverType>
